d8db918f04302138e0ebc93b1e85245d2a35ad5e
Shuhuai Cao <csh980717@outlook.com>
2021-12-10 10:06:29 -0500

    Dec. 10

--- Config.php
+++ Config.php
@@ -2,9 +2,9 @@
 
 class Config
 {
-    static string $ip = '18.118.188.234';
-    static string $port = '3306';
-    static string $username = 'root';
-    static string $password = 'cmpsc_431W';
-    static string $database = '431w_project';
+    public static string $ip = '18.118.188.234';
+    public static string $port = '3306';
+    public static string $username = 'root';
+    public static string $password = 'cmpsc_431W';
+    public static string $database = '431w_project';
 }
--- search_medicine.php
+++ search_medicine.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Search Patient</title>
+    <title>Search Medicine</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
             crossorigin="anonymous"></script>
@@ -21,8 +21,8 @@
 </header>
 <div class="container">
     <div class="row">
-        <div class="col">
-            <h2>Select a patient</h2>
+        <div class="col-4">
+            <h2>Select a medicine</h2>
         </div>
     </div>
     <div class="row">
@@ -30,41 +30,46 @@
             <table class="table table-hover">
                 <thead>
                 <tr>
-                    <th>Name &nbsp;<a href="#">Sort by name...</a></th>
-                    <th>Age</th>
-                    <th>Email</th>
+                    <th>Name&nbsp;<a href="#">Sort by name...</a></th>
+                    <th>Price</th>
+                    <th>In stock</th>
+                    <th>Quantity</th>
                     <th></th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
-                    <td>Nico Yazawa</td>
-                    <td>17</td>
-                    <td>example@example.com</td>
+                    <td>Medicine1</td>
+                    <td>$1.00</td>
+                    <td>999</td>
+                    <td class="w-25"><input class="form-control" type="number"></td>
                     <td>
                         <button class="btn btn-sm btn-primary">Select</button>
                     </td>
                 </tr>
                 <tr>
-                    <td>Rin Hoshizora</td>
-                    <td>15</td>
-                    <td>example2@example.com</td>
+                    <td>Medicine2</td>
+                    <td>$30.00</td>
+                    <td>255</td>
+                    <td class="w-25"><input class="form-control" type="number"></td>
                     <td>
                         <button class="btn btn-sm btn-primary">Select</button>
                     </td>
                 </tr>
                 <tr>
-                    <td>Keke Tang</td>
-                    <td>15</td>
-                    <td>example3@example.com</td>
+                    <td>Medicine3</td>
+                    <td>$45.00</td>
+                    <td>127</td>
+                    <td class="w-25"><input class="form-control" type="number"></td>
                     <td>
                         <button class="btn btn-sm btn-primary">Select</button>
                     </td>
                 </tr>
                 <tr>
-                    <td>Emma Verde</td>
-                    <td>17</td>
-                    <td>example4@example.com</td>
+                    <td>Medicine4</td>
+                    <td>$6.00</td>
+                    <td>65</td>
+                    <td class="w-25"><input class="form-control" type="number"></td>
                     <td>
                         <button class="btn btn-sm btn-primary">Select</button>
                     </td>
