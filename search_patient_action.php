8ac79b9bafa447a2bf9b1688a52b9aa9c9c91928
Shuhuai Cao <csh980717@outlook.com>
2021-12-11 14:38:25 -0500

    Search patient action

---
+++ search_patient_action.php
@@ -0,0 +1,46 @@
+<?php
+require 'Config.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$username = Config::$username;
+$password = Config::$password;
+$host = Config::$ip;
+$dbname = Config::$database;
+$keyword = "";
+$page = 1;
+
+if (isset($_GET['keyword'])) {
+    $keyword = $_GET['keyword'];
+}
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+} catch (PDOException $e) {
+    die("Could not connect to the database $dbname :" . $e->getMessage());
+}
+
+header('Content-Type: application/json');
+
+$min = ($page - 1) * 10 + 1;
+
+if ($keyword != "") {
+    $sql = $pdo->prepare('SELECT patient_id,fname,mname,lname, birthday, email FROM patient 
+where CONCAT(fname,\' \',lname) like :keyword or CONCAT(fname,\' \',mname,\' \',lname) like :keyword
+limit :min,10');
+    $sql->bindParam(':min', $min);
+    $str = '%' . $keyword . '%';
+    $sql->bindParam(':keyword', $str, PDO::PARAM_STR);
+} else {
+    $sql = $pdo->prepare('SELECT patient_id,fname,mname,lname, birthday, email FROM patient limit :min , 10');
+    $sql->bindParam(':min', $min);
+}
+$q = $sql->execute();
+$sql->setFetchMode(PDO::FETCH_ASSOC);
+
+$result = $sql->fetchAll();
+
+echo json_encode($result);
