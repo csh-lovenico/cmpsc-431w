2a45e6fe3b8b5b56639d8a9977ecc2e56cb873ad
Shuhuai Cao <csh980717@outlook.com>
2021-12-11 14:58:23 -0500

    Insert history

--- pat_center_allergy_history_insert.php
+++ pat_center_allergy_history_insert.php
@@ -9,6 +9,12 @@ $password = Config::$password;
 $host = Config::$ip;
 $dbname = Config::$database;
 
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    die('invalid user id');
+}
+$patient_id = $_SESSION['user_id'];
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 } catch (PDOException $e) {
@@ -16,7 +22,7 @@ try {
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title>PHP MySQL Query Data Demo</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
@@ -29,28 +35,28 @@ try {
 <body>
 <p>
     <?php
-    $sql = 'INSERT INTO allergy_history (patient_id, allergy_name, description) ';
-    $sql = $sql . 'VALUES ("'.$_POST["patient_id"] . '","' . $_POST["allergy_name"] . '","' . $_POST["adescription"] . '")';
-
+    //    $sql = 'INSERT INTO allergy_history (patient_id, allergy_name, description) ';
+    //    $sql = $sql . 'VALUES ("' . $patient_id . '","' . $_POST["allergy_name"] . '","' . $_POST["adescription"] . '")';
+    //    $sql = $pdo->prepare('INSERT into allergy_history(patient_id, allergy_name, description) VALUES (:patient_id,:allergy_name,:description)');
     try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->exec($sql);
+    $sql = $pdo->prepare('INSERT into allergy_history(patient_id, allergy_name, description) VALUES (:patient_id,:allergy_name,:description)');
+    //    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    //    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //    $conn->exec($sql);
+    $sql->execute(["patient_id" => $patient_id, "allergy_name" => $_POST["allergy_name"], "description" => $_POST["adescription"]]);
     echo "New record created successfully";
     ?>
 <p>You will be redirected in 3 seconds</p>
 <script>
-    var timer = setTimeout(function() {
-        window.location='pat_center.php'
+    var timer = setTimeout(function () {
+        window.location = 'pat_center.php'
     }, 3000);
 </script>
 <?php
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
 }
 $conn = null;
 ?>
-</p>
 </body>
-</div>
 </html>
--- pat_center_medical_history_insert.php
+++ pat_center_medical_history_insert.php
@@ -9,6 +9,12 @@ $password = Config::$password;
 $host = Config::$ip;
 $dbname = Config::$database;
 
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    die('invalid user id');
+}
+$patient_id = $_SESSION['user_id'];
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 } catch (PDOException $e) {
@@ -16,7 +22,7 @@ try {
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title>PHP MySQL Query Data Demo</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
@@ -29,29 +35,29 @@ try {
 <body>
 <p>
     <?php
-    $sql = 'INSERT INTO medical_history (patient_id, disease_name, description) ';
-    $sql = $sql . 'VALUES ("'.$_POST["patient_id"] . '","' . $_POST["disease_name"] . '","' . $_POST["description"] . '")';
+    //    $sql = 'INSERT INTO medical_history (patient_id, disease_name, description) ';
+    //    $sql = $sql . 'VALUES ("'.$_POST["patient_id"] . '","' . $_POST["disease_name"] . '","' . $_POST["description"] . '")';
 
     try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->exec($sql);
+    $sql = $pdo->prepare('INSERT into medical_history(patient_id, disease_name, description) VALUES (:patient_id,:disease_name,:description)');
+    $sql->execute(["patient_id" => $patient_id, "disease_name" => $_POST["disease_name"], "description" => $_POST["description"]]);
+    //    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    //    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //    $conn->exec($sql);
     echo "New record created successfully";
     ?>
 <p>You will be redirected in 3 seconds</p>
 <script>
-    var timer = setTimeout(function() {
-        window.location='pat_center.php'
+    var timer = setTimeout(function () {
+        window.location = 'pat_center.php'
     }, 3000);
 </script>
 <?php
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
 }
 $conn = null;
 ?>
-</p>
 </body>
-</div>
 </html>
 
