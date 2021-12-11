518d1fde085f41e40e637afe85a3e76154f4a796
Shuhuai Cao <csh980717@outlook.com>
2021-12-11 17:46:27 -0500

    Edit prescription page(unfinished)

--- edit_prescription.php
+++ edit_prescription.php
@@ -9,6 +9,13 @@ $password = Config::$password;
 $host = Config::$ip;
 $dbname = Config::$database;
 
+if (!isset($_GET['id'])) {
+    $new_mode = true;
+}
+
+$pat_id = $_GET['patid'];
+$doc_id = $_GET['docid'];
+$app_id = $_GET['id'];
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 } catch (PDOException $e) {
@@ -30,7 +37,7 @@ try {
 <body>
 <?php
 try {
-$sql = $pdo->prepare('SELECT a.attendence_date as attendence_date, 
+    $sql = $pdo->prepare('SELECT a.attendence_date as attendence_date, 
        a.comment as comment,
        p.fname as fname, p.mname as mname, p.lname as lname, p.birthday as birthday,
        d.dname as dname, 
@@ -39,11 +46,25 @@ $sql = $pdo->prepare('SELECT a.attendence_date as attendence_date,
        dr.name as drname, dr.usage as description, dr.price as price,
        pre.number as num, pre.prescription_id as prescription_id
         FROM attendence a, patient p, department d,level l,doctor dc, prescription pre, drug dr
-    limit 1');
-$q = $sql->execute([]);
-$sql->setFetchMode(PDO::FETCH_ASSOC);
+        where a.patient_id=p.patient_id and a.doctor_id=dc.doctor_id and pre.attendence_id=a.attendance_id and dr.drug_id = pre.drug_id and attendance_id=:aid');
+    $q = $sql->execute(['aid' => $app_id]);
+    $sql->setFetchMode(PDO::FETCH_ASSOC);
+
+    $result = $sql->fetchAll();
+
+    $sql = $pdo->prepare('select * from patient where patient_id = :pid');
+    $sql->execute(['pid' => $pat_id]);
+    $pat_info = $sql->fetch();
+
+    $sql = $pdo->prepare('select l.level_name as lvname, d.lname as dlname, d.fname as dfname, d.mname as dmname, dp.dname as dpname from doctor d, department dp, level l where d.level = l.level_id and dp.department_id = d.department_id and d.doctor_id = :did');
+    $sql->execute(['did' => $doc_id]);
+    $doc_info = $sql->fetch();
+
+
+} catch (PDOException $e) {
+    echo $sql->queryString . "<br>" . $e->getMessage();
+}
 ?>
-<?php while ($row = $sql->fetch()): ?>
 <header>
     <nav class="navbar navbar-light bg-light">
         <div class="container-fluid">
@@ -61,11 +82,14 @@ $sql->setFetchMode(PDO::FETCH_ASSOC);
             <table class="table">
                 <tr>
                     <th scope="row">Date</th>
-                    <td><?php echo htmlspecialchars($row['attendence_date']); ?></td>
+                    <td><?php echo htmlspecialchars($result[0]['attendence_date']); ?></td>
                 </tr>
                 <tr>
                     <th scope="row">Comment</th>
-                    <td><form class="d-flex"><input class="form-control me-2" type="text"><input type="submit" class="btn btn-sm btn-primary"></form>
+                    <td>
+                        <form class="d-flex"><input class="form-control me-2" type="text"><input type="submit"
+                                                                                                 class="btn btn-sm btn-primary">
+                        </form>
                     </td>
                 </tr>
             </table>
@@ -80,18 +104,18 @@ $sql->setFetchMode(PDO::FETCH_ASSOC);
                 <tr>
                     <th scope="row">Name
                     </th>
-                    <td><?php echo htmlspecialchars($row['fname']).' '. htmlspecialchars($row['mname']). ' ' .htmlspecialchars($row['lname']) ?></td>
+                    <td><?php echo htmlspecialchars($pat_info['fname']) . ' ' . htmlspecialchars($pat_info['mname']) . ' ' . htmlspecialchars($pat_info['lname']) ?></td>
                     </td>
                 </tr>
                 <tr>
                     <th scope="row">Age
                     </th>
-                    <td><?php echo 2021 - substr(htmlspecialchars($row['birthday']), 0, 4); ?></td>
+                    <td><?php echo 2021 - substr(htmlspecialchars($pat_info['birthday']), 0, 4); ?></td>
                 </tr>
                 <tr>
                     <th scope="row">Birthday
                     </th>
-                    <td><?php echo htmlspecialchars($row['birthday']); ?></td>
+                    <td><?php echo htmlspecialchars($pat_info['birthday']); ?></td>
                 </tr>
             </table>
         </div>
@@ -105,17 +129,17 @@ $sql->setFetchMode(PDO::FETCH_ASSOC);
                 <tr>
                     <th scope="row">Name
                     </th>
-                    <td><?php echo htmlspecialchars($row['dfname']).' '. htmlspecialchars($row['dmname']). ' ' .htmlspecialchars($row['dlname']) ?></td>
+                    <td><?php echo htmlspecialchars($doc_info['dfname']) . ' ' . htmlspecialchars($doc_info['dmname']) . ' ' . htmlspecialchars($doc_info['dlname']) ?></td>
                 </tr>
                 <tr>
                     <th scope="row">Department
                     </th>
-                    <td><?php echo htmlspecialchars($row['dname']); ?></td>
+                    <td><?php echo htmlspecialchars($doc_info['dpname']); ?></td>
                 </tr>
                 <tr>
                     <th scope="row">Level
                     </th>
-                    <td><?php echo htmlspecialchars($row['level_name']); ?></td>
+                    <td><?php echo htmlspecialchars($doc_info['lvname']); ?></td>
                 </tr>
             </table>
 
@@ -136,33 +160,25 @@ $sql->setFetchMode(PDO::FETCH_ASSOC);
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
+                <?php foreach ($result as $value) { ?>
+                    <tr>
 
-                    <td><?php echo htmlspecialchars($row['drname']); ?></td>
-                    <td><?php echo htmlspecialchars($row['num']); ?></td>
-                    <td><?php echo htmlspecialchars($row['price']); ?></td>
-                    <td><?php echo htmlspecialchars($row['description']); ?></td>
-                    <td>
-                            <?php echo '<form action="edit_prescription_delect.php" method="post"><input class="btn btn-sm btn-danger" type="submit" value="Delete"><input type="hidden" name="prescription_id" value="' . htmlspecialchars($row['prescription_id']) . '"></form>'; ?>
+                        <td><?php echo htmlspecialchars($value['drname']); ?></td>
+                        <td><?php echo htmlspecialchars($value['num']); ?></td>
+                        <td><?php echo htmlspecialchars($value['price']); ?></td>
+                        <td><?php echo htmlspecialchars($value['description']); ?></td>
+                        <td>
+                            <?php echo '<form action="edit_prescription_delect.php" method="post"><input class="btn btn-sm btn-danger" type="submit" value="Delete"><input type="hidden" name="prescription_id" value="' . htmlspecialchars($value['prescription_id']) . '"></form>'; ?>
 
-                    </td>
-                </tr>
+                        </td>
+                    </tr>
+                <?php } ?>
                 </tbody>
             </table>
-            <input class="btn btn-primary" type="button" value="Add medicine" action="search_medicine.php">
-
-
-
-
+            <input class="btn btn-primary" type="button" value="Add medicine"
+                   onclick="location.href='search_medicine.php?appid=<?php echo $app_id ?>'">
         </div>
     </div>
 </div>
-    <?php endwhile; ?>
-    <?php
-} catch(PDOException $e) {
-    echo $sql->queryString . "<br>" . $e->getMessage();
-}
-$conn = null;
-?>
 </body>
 </html>
