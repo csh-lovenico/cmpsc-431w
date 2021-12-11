fb7865ef71bc79365251ae5582fd726520765ccc
wangsimin <taoyiran815579058@outlook.com>
2021-12-11 14:19:03 -0500

    Search-patient update

--- js/search_patient.js
+++ js/search_patient.js
@@ -2,87 +2,79 @@ function getEle(id) {
     return document.getElementById(id);
 }
 
-var cou = 1;
-function sort_app_record(user_id) {
+var patient_name = '';
+function search_patient_by_name() {
+    var patient_name = getEle("patient_name").value;
+    var request = new XMLHttpRequest();
+    request.open("GET","doc_center_action.php?func=" + 1 + "&patient_name=" + patient_name); //async
+    request.send();
+    request.onreadystatechange = function() {
+        if(request.readyState === 4) {
+            if(request.status === 200) {
+                var jsonStr = request.response.replace(new RegExp('\\"',"gm"), '"' );
+                var a = JSON.parse(jsonStr);
+                var obj = getEle("search_patient_table_body");
+                var table = '';
+
+                for (var k in a) {
+                    for (var j in a[k]) {
+                        table += '' +
+                            '                <tr>\n' +
+                            '                    <td>' + a[k][j].name + '</td>\n' +
+                            '                    <td>' + a[k][j].age + '</td>\n' +
+                            '                    <td>' + a[k][j].email + '</td>\n' +
+                            '                    <td>\n' +
+                            '                        <div>\n' +
+                            '                            <button type="button" class="btn btn-sm btn-secondary">Select</button>\n' +
+                            '                        </div>\n' +
+                            '                    </td>\n' +
+                            '                </tr>'
+                    }
+                }
+                obj.innerHTML = table;
+            }
+            else {
+                alert("error occured: " + request.status);
+            }
+        }
+    }
+}
+
+var cou = 0;
+function sort_patient() {
     var request = new XMLHttpRequest();
     ++cou;
-    request.open("GET","doc_center_action.php?func=" + 1 + "&user_id=" + user_id + "&mode=" + cou); //async
+    if (patient_name == '') {
+        // not init
+        request.open("GET","doc_center_action.php?func=" + 1 + "&patient_name=" + patient_name + "&mode=" + cou); //async
+    } else {
+        request.open("GET","doc_center_action.php?func=" + 1 + "&patient_name=" + patient_name + "&mode=" + cou); //async
+    }
     request.send();
     request.onreadystatechange = function() {
         if(request.readyState === 4) {
             if(request.status === 200) {
                 var jsonStr = request.response.replace(new RegExp('\\"',"gm"), '"' );
                 var a = JSON.parse(jsonStr);
-                var obj = getEle("app_table_body");
+                var obj = getEle("search_patient_table_body");
                 var table = '';
 
                 for (var k in a) {
                     for (var j in a[k]) {
                         table += '' +
                             '                <tr>\n' +
-                            '                    <td>' + a[k][j].app_date + '</td>\n' +
-                            '                    <td>' + a[k][j].patient_name + '</td>\n' +
+                            '                    <td>' + a[k][j].name + '</td>\n' +
+                            '                    <td>' + a[k][j].age + '</td>\n' +
+                            '                    <td>' + a[k][j].email + '</td>\n' +
                             '                    <td>\n' +
                             '                        <div>\n' +
-                            '                            <button type="button" class="btn btn-sm btn-secondary">Detail</button>\n' +
+                            '                            <button type="button" class="btn btn-sm btn-secondary">Select</button>\n' +
                             '                        </div>\n' +
                             '                    </td>\n' +
                             '                </tr>'
                     }
                 }
                 obj.innerHTML = table;
-            }
-            else {
-                alert("error occured: " + request.status);
-            }
-        }
-    }
-}
-
-function get_app_record(user_id) {
-    var request = new XMLHttpRequest();
-    request.open("GET","doc_center_action.php?func=" + 3 + "&user_id=" + user_id); //async
-    request.send();
-    request.onreadystatechange = function() {
-        if(request.readyState === 4) {
-            if(request.status === 200) {
-                var jsonStr = request.response.replace(new RegExp('\\"',"gm"), '"' );
-                var a = JSON.parse(jsonStr);
-                var obj = getEle("app_table_body");
-                var table = '';
-
-                for (var k in a) {
-                    for (var j in a[k]) {
-                        table += '' +
-                            '                <tr>\n' +
-                            '                    <td>' + a[k][j].app_date + '</td>\n' +
-                            '                    <td>' + a[k][j].patient_name + '</td>\n' +
-                            '                    <td>\n' +
-                            '                        <div>\n' +
-                            '                            <button type="button" class="btn btn-sm btn-secondary">Detail</button>\n' +
-                            '                        </div>\n' +
-                            '                    </td>\n' +
-                            '                </tr>'
-                    }
-                }
-                obj.innerHTML = table;
-            }
-            else {
-                alert("error occured: " + request.status);
-            }
-        }
-    }
-}
-
-
-function add_app_record(user_id) {
-    var request = new XMLHttpRequest();
-    request.open("GET","doc_center_action.php?func=" + 2 + "&user_id=" + user_id); //async
-    request.send();
-    request.onreadystatechange = function() {
-        if(request.readyState === 4) {
-            if(request.status === 200) {
-                window.alert(request.response)
             }
             else {
                 alert("error occured: " + request.status);
