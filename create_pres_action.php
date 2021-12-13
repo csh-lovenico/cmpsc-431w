ba661d2fdaa1a77cfe5df83a9bef7d891ae3c9b8
Shuhuai Cao <csh980717@outlook.com>
2021-12-12 22:52:12 -0500

    Change default timezone

--- create_pres_action.php
+++ create_pres_action.php
@@ -4,7 +4,7 @@ require 'Config.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+date_default_timezone_set('America/New_York');
 $username = Config::$username;
 $password = Config::$password;
 $host = Config::$ip;
