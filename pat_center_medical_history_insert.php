f442b406f0739a1125f0998df7ca06ee69087360
Shuhuai Cao <csh980717@outlook.com>
2021-12-13 13:25:36 -0500

    Revert "Delete pat_center_medical_history_delete & insert.php"
    This reverts commit d4ae61d6a1521193d4bcd8f968b13c7623b64d88.

---
+++ pat_center_medical_history_insert.php
@@ -0,0 +1,63 @@
+<?php
+require 'Config.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$username = Config::$username;
+$password = Config::$password;
+$host = Config::$ip;
+$dbname = Config::$database;
+
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    die('invalid user id');
+}
+$patient_id = $_SESSION['user_id'];
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+} catch (PDOException $e) {
+    die("Could not connect to the database $dbname :" . $e->getMessage());
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP MySQL Query Data Demo</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+<p>
+    <?php
+    //    $sql = 'INSERT INTO medical_history (patient_id, disease_name, description) ';
+    //    $sql = $sql . 'VALUES ("'.$_POST["patient_id"] . '","' . $_POST["disease_name"] . '","' . $_POST["description"] . '")';
+
+    try {
+    $sql = $pdo->prepare('INSERT into medical_history(patient_id, disease_name, description) VALUES (:patient_id,:disease_name,:description)');
+    $sql->execute(["patient_id" => $patient_id, "disease_name" => $_POST["disease_name"], "description" => $_POST["description"]]);
+    //    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    //    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //    $conn->exec($sql);
+    echo "New record created successfully";
+    ?>
+<p>You will be redirected in 3 seconds</p>
+<script>
+    var timer = setTimeout(function () {
+        window.location = 'pat_center.php'
+    }, 3000);
+</script>
+<?php
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+$conn = null;
+?>
+</body>
+</html>
+
