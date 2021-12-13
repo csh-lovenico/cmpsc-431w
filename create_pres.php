8e09095f490b619b3df8d9cfde53b9bb94e819f3
wangsimin <taoyiran815579058@outlook.com>
2021-12-12 22:50:28 -0500

    Search-patient update

--- create_pres.php
+++ create_pres.php
@@ -45,6 +45,7 @@ try {
             crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="js/create_pres_action.js"></script>
 </head>
 <body>
 <header>
@@ -105,11 +106,12 @@ try {
             <div class="mb-3">
                 <textarea class="form-control" id="comment" name="comment"></textarea>
             </div>
-            <input type="submit" class="btn btn-primary" value="Submit">
+            <input type="button" class="btn btn-primary" onclick=submit_comment() value="Submit">
         </form>
     </div>
 </div>
 </div>
+<p style="display: none" id="pid"><?php echo $pid?></p>
 </body>
 </html>
 
---
+++ js/create_pres_action.js
@@ -0,0 +1,23 @@
+function getEle(id) {
+    return document.getElementById(id);
+}
+
+function submit_comment() {
+    var request = new XMLHttpRequest();
+    var comment = getEle("comment").value;
+    var pid = getEle("pid").innerHTML;
+
+    request.open("GET","create_pres_action.php?pid=" + pid + "&comment=" + comment); //async
+    request.send();
+    request.onreadystatechange = function() {
+        if(request.readyState === 4) {
+            if(request.status === 200) {
+                alert(request.response)
+                location.replace('edit_prescription.php?id=' + request.response);
+            }
+            else {
+                alert("error occured: " + request.status);
+            }
+        }
+    }
+}
